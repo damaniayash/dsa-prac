8154e48377070a945d9633e2c192cd73680322ba
damaniayash <damaniayash@gmail.com>
2021-12-04 20:19:46 +0530

    duplicates

---
+++ array/ud_duplicate.py
@@ -0,0 +1,12 @@
+arr = [1,2,3,1,5,1,2,3,4,5,6,4,32,2,1,22,2,31,4,325,325,435,45,4,5,45,45,45,4,5,5,4]
+counter = {}
+out=[]
+for i in range(0,len(arr)):
+    counter[arr[i]] = 0
+for i in range(0,len(arr)):
+    counter[arr[i]] = counter[arr[i]] + 1
+for i in counter:
+    if counter[i] > 1:
+        out.append(i)
+print("The repeated elements are : ",out)
+
