bba3855be17593e3aa29f69d5447e7e8de7ec393
damaniayash <damaniayash@gmail.com>
2022-03-27 15:22:33 +0530

    comments

--- stack_and_queue/clone_graph.py
+++ stack_and_queue/clone_graph.py
@@ -10,21 +10,16 @@ class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
         if not node:
             return
-        visited = []
-        node_dict = {}
+        visited, node_dict = [], {}
+        #node_dict contains original node as key and new node as value.
         new_node = Node(node.val)
         self.helper(node,visited,new_node,node_dict)
-        # for k, v in node_dict.items():
-        #     print(k,v)
-        #     print(k.val, v.val)
         return new_node
         
     def helper(self,node,visited,new_node,node_dict):
-        #print(node.val)
         visited.append(node)
         curr_node = new_node
         node_dict[node] = curr_node
-        #[print('vis',i.val) for i in visited]
         for i in node.neighbors:
             if i in visited:
                 curr_node.neighbors.append(node_dict[i])
@@ -32,7 +27,6 @@ class Solution:
             else: #i is not encountered before
                 temp = Node(i.val)
                 curr_node.neighbors.append(temp)
-                #temp.neighbors.append(curr_node)
                 self.helper(i,visited,temp,node_dict) 
                     
         
