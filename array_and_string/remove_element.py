626c6628e199a6d82a6da06b3da7c2f6c83e17e4
Yash Damania <45127647+damaniayash@users.noreply.github.com>
2022-04-28 15:41:07 +0530

    Update remove_element.py
    updated remove element

--- array_and_string/remove_element.py
+++ array_and_string/remove_element.py
@@ -1,9 +1,15 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        count = 0
-        for i in range(0,len(nums)):
-            if nums[i] == val:
-                count += 1
-        for _ in range(0, count):
-            nums.remove(val)
-        return len(nums)
+        ptr = 0
+        for i in range(0, len(nums)):
+            if nums[i] != val:
+                nums[ptr] = nums[i]
+                ptr+=1
+        return ptr
+        # count = 0
+        # for i in range(0,len(nums)):
+        #     if nums[i] == val:
+        #         count += 1
+        # for _ in range(0, count):
+        #     nums.remove(val)
+        # return len(nums)
