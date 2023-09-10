40ccc3cd1454d838c4f148b3aeb528f350927225
Yash Damania <45127647+damaniayash@users.noreply.github.com>
2023-09-10 01:35:25 -0700

    added comments to subtree of another tree

--- blind75/trees/subtree_of_another_tree.py
+++ blind75/trees/subtree_of_another_tree.py
@@ -4,6 +4,19 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+'''
+Make sure you understand same tree problem first.
+if subRoot is empty -> It can still be considered as subtree of root (leaf nodes)
+if root is empty and subRoot is not -> root cannot contain subroot in this case so return False
+if the root.val and subRoot.val are equal we can call the sametree function
+    if that function return true that means the subtrees are identical
+
+we want to traverse the entire root tree and see whether any subroot tree nodes are equal to the current node
+then call same tree and return true is thet are the same.
+
+Overall last line means the if either left od the right subtree of the root tree is equal to the subroot tree we can return true.
+this is tagged as easy but i think it should be medium
+'''
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
         def sametree(p, q):
@@ -14,14 +27,15 @@ class Solution:
             if p.val != q.val:
                 return False
             return sametree(p.left, q.left) and sametree(p.right, q.right)
-            
-        if not root:
-            return False
-        if sametree(root, subRoot):
-            return True
+
+        if not subRoot: return True
+        if not root: return False
+        if root.val == subRoot.val:
+            if sametree(root, subRoot):
+                return True
         return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
 
-        
+                
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
