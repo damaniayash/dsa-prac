3a2a892c7cc7b7ad23ea09cf8d15eea336d03233
Yash Damania <45127647+damaniayash@users.noreply.github.com>
2023-09-13 23:06:36 -0700

    added kth largest element in array using minheap

---
+++ blind75/heap/kth_largest_element_in_an_array.py
@@ -0,0 +1,14 @@
+'''
+Use minheap.
+'''
+import heapq
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = []
+        heapq.heapify(heap)
+        for num in nums:
+            if len(heap) == k:
+                heapq.heappushpop(heap, num)
+            else:
+                heapq.heappush(heap, num)
+        return heap[0]
